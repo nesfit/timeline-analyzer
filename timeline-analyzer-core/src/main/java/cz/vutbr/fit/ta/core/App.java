2f23bf2f7b535478c8b06f20bdf6000131b8e9d2
Radek Burget <burgetr@fit.vutbr.cz>
2017-07-24 16:58:28 +0200

    Initial commit

---
+++ timeline-analyzer-core/src/main/java/cz/vutbr/fit/ta/core/App.java
@@ -0,0 +1,13 @@
+package cz.vutbr.fit.ta.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
