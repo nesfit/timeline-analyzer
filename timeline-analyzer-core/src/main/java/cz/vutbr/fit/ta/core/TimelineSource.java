c4bcfeb5fc8962936db9295b94675db00e44a2cb
Radek Burget <burgetr@fit.vutbr.cz>
2017-07-28 13:23:23 +0200

    Timeline source abstraction

---
+++ timeline-analyzer-core/src/main/java/cz/vutbr/fit/ta/core/TimelineSource.java
@@ -0,0 +1,19 @@
+/**
+ * TimelineSource.java
+ *
+ * Created on 28. 7. 2017, 13:15:10 by burgetr
+ */
+package cz.vutbr.fit.ta.core;
+
+import cz.vutbr.fit.ta.ontology.Timeline;
+
+/**
+ * 
+ * @author burgetr
+ */
+public abstract class TimelineSource
+{
+
+    abstract public Timeline getTimeline();
+    
+}
