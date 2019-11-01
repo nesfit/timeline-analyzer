1e5ab87e623bab5530e6fb1e18b5a92df953b736
Radek Burget <burgetr@fit.vutbr.cz>
2019-11-01 14:28:46 +0100

    Packaging script

---
+++ misc/packages.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+zip ../ClientApps.zip ../timeline-analyzer-fb/target/DownloadFB.jar ../timeline-analyzer-twitter/target/DownloadTwitter.jar ../timeline-analyzer-local/target/DecodeProfiles.jar
+cp ../timeline-client-angular/target/ta.war ../
